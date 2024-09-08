cc423f2f0ba5f972e0aeb78009cd6d7e3bccf662
shinas07 <shinasamanpk@gmail.com>
2024-09-08 22:22:05 +0530

    Update accounts

--- Server/accounts/models.py
+++ Server/accounts/models.py
@@ -1,5 +1,89 @@
-from django.db import models
+# from django.db import models
+# from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+# from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
 
+    
+# class CustomUser(AbstractUser):
+#      # Remove the username field since we'll use email as the unique identifier
+#     username = None
+#     email = models.EmailField(unique=True)
+#     is_verified = models.BooleanField(default=True)
+#     firstname = models.CharField(max_length=30, null=True, blank=True)
+#     lastname = models.CharField(max_length=30, null=True, blank=True)
+
+#     username = None
+#      # Use email as the unique identifier
+#     USERNAME_FIELD = 'email'
+#     REQUIRED_FIELDS = ['first_name', 'last_name']
+
+
+
+
+
+
+
+
+
+# class User(AbstractUser):
+#     first_name = models.CharField(max_length=100)
+#     last_name = models.CharField(max_length=100)
+#     email = models.EmailField(unique=True)
+#     password = models.CharField(max_length=100)
+#     # confirm_password = models.CharField(max_length=100)
+    
+#     USERNAME_FIELD = 'email'
+#     REQUIRED_FIELDS = ['first_name', 'last_name', 'password']
+
+
+
+
+
+
+
+
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+
+class StudentUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError(_('The Email field must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+class StudentUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('email address'), unique=True)
+    password = models.CharField(max_length=128)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    objects = StudentUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        verbose_name = 'Student'
+        verbose_name_plural = 'Students'
+
+
+
+
+
+class OTPVerification(models.Model):
+    email = models.EmailField()
+    otp = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
