f9733e650eecffe0c34c9147b23fa19f8979582b
shinas07 <shinasamanpk@gmail.com>
2024-09-08 22:18:00 +0530

    Set up Redux store configuration for state management

---
+++ Client/lexiwaves/src/store/authSlice.jsx
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  user: null,
+  token: localStorage.getItem('token'),
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    setCredentials: (state, action) => {
+      const { user, token } = action.payload;
+      state.user = user;
+      state.token = token;
+      localStorage.setItem('token', token);
+    },
+    logout: (state) => {
+      state.user = null;
+      state.token = null;
+      localStorage.removeItem('token');
+    },
+  },
+});
+
+export default authSlice;
