2a758b633686dea9b779abaef0337bba554f5cb9
shinas07 <shinasamanpk@gmail.com>
2024-09-10 21:03:56 +0530

    Update tutor/urls.py

--- Server/tutor/urls.py
+++ Server/tutor/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import  TutorSignUpView, TutorVerifyEmailView, TutorLoginView, TutorDetails
+from .views import  TutorSignUpView, TutorVerifyEmailView, TutorLoginView, TutorDetails, TutorListView
 
 urlpatterns = [
     path('signup/', TutorSignUpView.as_view(), name='tutor_signup'),
     path('verify_email/', TutorVerifyEmailView.as_view(), name='verify_email'),
     path('login/', TutorLoginView.as_view(), name='tutor-login'),
     path('details/',TutorDetails.as_view(), name='tutor-details'),
+    path('tutor-list/',TutorListView.as_view(), name='tutor-list'),
+    
 ]
