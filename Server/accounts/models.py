0fd5f4d96ff5c1120157356532181b940e7bc33a
shinas07 <shinasamanpk@gmail.com>
2024-09-12 16:57:46 +0530

    made chagne in modal and serilaizer

--- Server/accounts/models.py
+++ Server/accounts/models.py
@@ -1,82 +1,101 @@
-# from django.db import models
-# from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-# from django.utils.translation import gettext_lazy as _
-
-# Create your models here.
-
-
-    
-# class CustomUser(AbstractUser):
-#      # Remove the username field since we'll use email as the unique identifier
-#     username = None
-#     email = models.EmailField(unique=True)
-#     is_verified = models.BooleanField(default=True)
-#     firstname = models.CharField(max_length=30, null=True, blank=True)
-#     lastname = models.CharField(max_length=30, null=True, blank=True)
-
-#     username = None
-#      # Use email as the unique identifier
-#     USERNAME_FIELD = 'email'
-#     REQUIRED_FIELDS = ['first_name', 'last_name']
-
-
-
-
-
-
-
-
-
-# class User(AbstractUser):
-#     first_name = models.CharField(max_length=100)
-#     last_name = models.CharField(max_length=100)
-#     email = models.EmailField(unique=True)
-#     password = models.CharField(max_length=100)
-#     # confirm_password = models.CharField(max_length=100)
-    
-#     USERNAME_FIELD = 'email'
-#     REQUIRED_FIELDS = ['first_name', 'last_name', 'password']
-
-
-
-
-
-
-
-
-from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-from django.utils.translation import gettext_lazy as _
+from django.db import models
 
-class StudentUserManager(BaseUserManager):
+# All user model
+class CustomUserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email:
-            raise ValueError(_('The Email field must be set'))
+            raise ValueError('The Email field must be set')
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-class StudentUser(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(_('email address'), unique=True)
-    password = models.CharField(max_length=128)
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
+    
+    USER_TYPE_CHOICES = (
+        ('student', 'Student'),
+        ('tutor', 'Tutor'),
+        ('admin', 'Admin'),
+    )
+    user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES)
 
-    objects = StudentUserManager()
+    objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'user_type']
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return self.email
 
-    class Meta:
-        verbose_name = 'Student'
-        verbose_name_plural = 'Students'
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    
+
+
+# for Studets
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='student_profile')
+    # Add student-specific fields here, for example:
+    # grade_level = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"Student: {self.user.get_full_name()}"
+
+    
+
+
+
+
+
+
+
+
+# from django.db import models
+# from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+# from django.utils.translation import gettext_lazy as _
+
+# class StudentUserManager(BaseUserManager):
+#     def create_user(self, email, password=None, **extra_fields):
+#         if not email:
+#             raise ValueError(_('The Email field must be set'))
+#         email = self.normalize_email(email)
+#         user = self.model(email=email, **extra_fields)
+#         user.set_password(password)
+#         user.save(using=self._db)
+#         return user
+
+# class StudentUser(AbstractBaseUser, PermissionsMixin):
+#     email = models.EmailField(_('email address'), unique=True)
+#     password = models.CharField(max_length=128)
+#     first_name = models.CharField(max_length=30)
+#     last_name = models.CharField(max_length=30)
+#     is_active = models.BooleanField(default=True)
+#     date_joined = models.DateTimeField(auto_now_add=True)
+
+#     objects = StudentUserManager()
+
+#     USERNAME_FIELD = 'email'
+#     REQUIRED_FIELDS = ['first_name', 'last_name']
+
+#     def __str__(self):
+#         return f"{self.first_name} {self.last_name}"
+
+#     class Meta:
+#         verbose_name = 'Student'
+#         verbose_name_plural = 'Students'
 
 
 
--- Server/accounts/serializers.py
+++ Server/accounts/serializers.py
@@ -1,17 +1,32 @@
 from rest_framework import serializers
-from .models import StudentUser
+from .models import User
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+from django.contrib.auth.password_validation import validate_password
 
+User = get_user_model()
 
-class StudentUserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+
     class Meta:
-        model = StudentUser
-        fields = ['first_name', 'last_name', 'email', 'password']
+        model = User
+        fields = ['email', 'first_name', 'last_name', 'password', 'user_type']
         extra_kwargs = {
-            'password': {'write_only': True},
+            'first_name': {'required': True},
+            'last_name': {'required': True},
+            'user_type': {'required': True}
         }
+
+
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
 
 
 class StudentListSerializer(serializers.ModelSerializer):
     class Meta:
-        model = StudentUser
+        model = User
         fields = '__all__'
