950bf82f0974ee9e6c6308287de92ab277e676d6
shinas07 <shinasamanpk@gmail.com>
2024-09-17 16:25:57 +0530

    Update serializers in lexi_admin for better API handling

--- Server/lexi_admin/serializers.py
+++ Server/lexi_admin/serializers.py
@@ -1,7 +1,38 @@
 from rest_framework import serializers
-from tutor.models import TutorDetails
+from tutor.models import TutorDetails 
+from accounts.models import User
+
+
+class StudentListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class TutorListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+        
+# class TutorDetailsListSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = TutorDetails
+#         fields = '__all__'
+
+
+# Tutor Details for request approval
+class TutorDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TutorDetails
+        fields = '__all__'
 
 class TutorDetailsSerializer(serializers.ModelSerializer):
     class Meta:
         model = TutorDetails
         fields = '__all__'
+
+class TutorRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
