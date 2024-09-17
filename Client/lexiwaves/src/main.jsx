d08a57a9a342be1618296a98da14c40894cdf766
shinas07 <shinasamanpk@gmail.com>
2024-09-17 16:25:14 +0530

    Modify main.jsx for application initialization changes

--- Client/lexiwaves/src/main.jsx
+++ Client/lexiwaves/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { store, persistor } from './redux/Store.jsx'; 
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+    <GoogleOAuthProvider clientId="862039732883-28g3c65vg9vj08d5antbeut9hmgvih97.apps.googleusercontent.com">
+      <App />
+      </GoogleOAuthProvider>
+    </PersistGate>
+  </Provider>
+);
