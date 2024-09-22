993e6288be2f71b2322fdcbe1b38c110b6486dd0
shinas07 <shinasamanpk@gmail.com>
2024-09-22 09:24:50 +0530

    feat: add imageGrid for home page

---
+++ Client/lexiwaves/src/pages/ImageGrid.jsx
@@ -0,0 +1,38 @@
+// import React from 'react';
+
+// const ImageGrid = () => {
+//   return (
+//     <div className="w-full md:w-1/2 grid grid-cols-4 grid-rows-3 gap-4 p-4">
+//       {/* Top left - Car view */}
+
+
+//       {/* Top center - Coffee */}
+    
+// \
+
+//       {/* Center - Main image */}
+//       <div className="col-span-6 row-span-1">
+//         <img
+//           src="https://learningforward.org/wp-content/uploads/2020/05/GettyImages-1220226088-scaled-1-2048x1366.jpg"
+//           alt="People chatting"
+//           className="w-full h-full object-cover rounded-lg"
+//         />
+//       </div>
+
+  
+
+  
+
+//       {/* Bottom right - Garden view */}
+//       <div className="col-span-1 row-span-1">
+//         <img
+//           src="https://learningforward.org/wp-content/uploads/2020/05/GettyImages-1220226088-scaled-1-2048x1366.jpg"
+//           alt="Garden view"
+//           className="w-full h-full object-cover rounded-lg"
+//         />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ImageGrid;
