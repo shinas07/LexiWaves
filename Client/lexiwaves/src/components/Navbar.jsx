228b7e1f8ed1451cf99eb8dca5f0815b54d7c709
shinas07 <shinasamanpk@gmail.com>
2024-09-23 08:55:40 +0530

    Improve Navbar component for better responsiveness

--- Client/lexiwaves/src/components/Navbar.jsx
+++ Client/lexiwaves/src/components/Navbar.jsx
@@ -1,9 +1,20 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FloatingNav } from "./ui/floatingNavbar";
 import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
+
 export default function FloatingNavbar() {
-    
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const checkLoginStatus = () => {
+      const token = localStorage.getItem('accessToken');
+      setIsLoggedIn(!!token);
+    };
+
+    checkLoginStatus();
+  }, []);
+
   const navItems = [
     {
       name: "Home",
@@ -11,21 +22,20 @@ export default function FloatingNavbar() {
       icon: <IconHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
     },
     {
-      name: "About",
-      link: "/about",
+      name: "Courses",
+      link: "/courses",
       icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
     },
     {
       name: "Contact",
       link: "/contact",
-      icon: (
-        <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
-      ),
+      icon: <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />,
     },
   ];
+
   return (
-    (<div className="relative  w-full">
-      <FloatingNav navItems={navItems} />
-    </div>)
+    <div className="relative w-full">
+      <FloatingNav navItems={navItems} isLoggedIn={isLoggedIn} />
+    </div>
   );
 }
