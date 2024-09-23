732b88db29ea4dd2a5c950b2df52f69cc2c2a187
shinas07 <shinasamanpk@gmail.com>
2024-09-23 08:55:39 +0530

    Refactor App component structure

--- Client/lexiwaves/src/App.jsx
+++ Client/lexiwaves/src/App.jsx
@@ -8,7 +8,7 @@ import {
 import SignUPForm from "./pages/UserPages.jsx/SignUpPage";
 import Home from "./pages/HomePage";
 import Login from "./pages/UserPages.jsx/SignInPage";
-import OtpVerification from "./pages/UserOtpPage";
+import OtpVerification from "./pages/UserPages.jsx/UserOtpPage";
 // import { Provider, useSelector} from 'react-redux';
 
 //User page imports
@@ -33,14 +33,25 @@ import StudentsPage from "./pages/admin/StudentsList";
 import Tutorlist from "./pages/admin/ApprovedTutorList";
 import TutorRequests from "./pages/admin/TutorRequests";
 import AdminSignup from "./pages/admin/SignIn";
-import CreateCoursePage from "./pages/TutorPags/CreateCourse";
+import CreateCoursePage from "./pages/TutorPags/CourseCreation";
 import TutorDetail from "./pages/admin/TutorDetail";
+import AdminLanguages from "./pages/admin/LanguageCreate";
 // import  CustomToaster  from './pages/CustomToaster';
 import { useSelector } from "react-redux";
+import CourseList from "./pages/UserPages.jsx/CoursePage";
 //Redux
 const App = () => {
-  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
-  console.log(isAuthenticated)
+  const isAuthenticatedUser = useSelector((state) => state.auth.isAuthenticated);
+  // const isAuthenticatedTutor = useSelector((state) => state.tutor.isAuthenticated);
+
+
+
+  const ProtectedRoute = ({ children }) => {
+    const token = localStorage.getItem('tutor');
+    return token ? <Navigate to="/tutor-dashboard" /> : children;
+  };
+
+
 
   return (
     <div className="dark">
@@ -49,55 +60,39 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/signin" element={isAuthenticated ? <Navigate to="/" /> : <Login />}/>
-          <Route path="/signup" element={isAuthenticated ? <Navigate to="/" /> : <SignUPForm />}/>
+          <Route path="/signup" element={isAuthenticatedUser ? <Navigate to="/" /> : <SignUPForm />}/>
+          <Route path="/signin" element={isAuthenticatedUser ? <Navigate to="/" /> : <Login />}/>
 
-          <Route path="/otp" element={isAuthenticated ? <Navigate to="/" /> : <OtpVerification />}/>
+          <Route path="/otp" element={isAuthenticatedUser ? <Navigate to="/" /> : <OtpVerification />}/>
+          
+          <Route path="/user-account" element={!isAuthenticatedUser ? <Navigate to="/signin" /> : <UserAccountPage />
+            }
+          />
+          <Route path="/courses" element={<CourseList/>}></Route>
+
           {/* Tutors routes here */}
 
-          <Route
-            path="/tutor-signup"
-            element={
-              isAuthenticated ? (
-                <Navigate to="/tutor-dashboard" />
-              ) : (
-                <TutorSignUP />
-              )
-            }
-          />
-          <Route path="/tutor-otp" element={<TutorOtpVerification />} />
-          {/* <Route
-              path="/tutor-signin"
-              element={
-                tutorLoggedIn ? (
-                  <Navigate to= />
-                ) : (
-                  <TutorLogin />
-                )
-              }
-            /> */}
-          <Route
-            path="/user-account"
-            element={
-              !isAuthenticated ? <Navigate to="/signin" /> : <UserAccountPage />
-            }
-          />
+
+
+       
 
           <Route
-            path="/tutor-signin"
-            element={
-              isAuthenticated ? (
-                <Navigate to="/tutor-dashboard" />
-              ) : (
-                <TutorLogin />
-              )
-            }
-          />
+          path="/tutor-signup"
+          element={<ProtectedRoute><TutorSignUP /></ProtectedRoute>}/>
+        
+        <Route
+          path="/tutor-otp"
+          element={<ProtectedRoute><TutorOtpVerification /></ProtectedRoute>}/>
+        <Route path="/tutor-signin" element={<ProtectedRoute><TutorLogin /></ProtectedRoute>}/>
+            <Route path="/tutor-details" element={<TutorDetails></TutorDetails>}></Route>
           <Route path="/tutor-dashboard" element={<TutorHomePage />} />
           <Route path="/tutor-setup" element={<TutorSetup />} />
           <Route path="/tutor-details" element={<TutorDetails />} />
           <Route path="/tutor-reqeusts" element={<TutorRequests />} />
           <Route path="/tutor-create-course" element={<CreateCoursePage />} />
+          
+
+
 
           {/* Admin routes here */}
           <Route path="/admin-login" element={<AdminSignup />} />
@@ -108,6 +103,7 @@ const App = () => {
             path="/admin-tutor-details/:tutorId"
             element={<TutorDetail />}
           />
+          <Route path="/admin-language" element={<AdminLanguages/>}/>
         </Routes>
       </Router>
     </div>
