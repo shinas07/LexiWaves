e3b861bf254babe89ba3406a53039805020a408d
shinas07 <shinasamanpk@gmail.com>
2024-10-22 16:48:57 +0530

    Configure ASGI for HTTP and WebSocket support

--- Server/lexiWaves/asgi.py
+++ Server/lexiWaves/asgi.py
@@ -6,11 +6,21 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
-
 import os
-
 from django.core.asgi import get_asgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lexiWaves.settings')
+django_asgi_app = get_asgi_application()
 
-application = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from chat.routing import websocket_urlpatterns
+
+application = ProtocolTypeRouter({
+    'http': django_asgi_app,
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    ),
+})
