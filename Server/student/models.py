47f9a955abddafcc6360b6497f273823522b2084
shinas07 <shinasamanpk@gmail.com>
2024-10-25 21:02:20 +0530

    add lesson complete model

--- Server/student/models.py
+++ Server/student/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from accounts.models import User
+from tutor.models import Lesson
 
 # Create your models here.
+
+# User Watched 
+class UserLesson(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    watched = models.BooleanField(default=False)
+    watched_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('user', 'lesson')
