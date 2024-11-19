fd3da5a87582f00c3644779f3605299bab79140e
shinas07 <shinasamanpk@gmail.com>
2024-11-19 10:59:02 +0530

    chore: remove StrictMode wrapper around App component

--- Client/lexiwaves/src/main.jsx
+++ Client/lexiwaves/src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
@@ -9,9 +8,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <StrictMode>
       <App />
-      </StrictMode>
     </PersistGate>
   </Provider>
 );
