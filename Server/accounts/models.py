4b9f3ebda38c9587d3de464dd3297b4982b74357
shinas07 <shinasamanpk@gmail.com>
2024-12-05 12:03:36 +0530

    feat(auth): add google_id field to User modal

--- Server/accounts/models.py
+++ Server/accounts/models.py
@@ -20,6 +20,7 @@ class CustomUserManager(BaseUserManager):
         return self.create_user(email, password, **extra_fields)
 
 class User(AbstractBaseUser, PermissionsMixin):
+    google_id = models.CharField(max_length=100, blank=True, null=True)
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
