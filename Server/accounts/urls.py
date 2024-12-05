a05ab275c7c15309884b3a35a3026554d5109c19
shinas07 <shinasamanpk@gmail.com>
2024-12-05 16:45:35 +0530

    feat: add webhook endpoint for strip

--- Server/accounts/urls.py
+++ Server/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 # from . views import  SignUpView, VerifyEmailView, UserLoginView,ResendOtpView, Student ,ResendOtpView,RefreshTokenView
-from . views import  SignUpView, VerifyEmailView, UserLoginView,ResendOtpView,ResendOtpView,CourseListView,CourseDetailView, CourseVideoView,CreateCheckoutSession, UserEnrolledCourses, WatchCourseView, CheckEnrollmentView, RequestotpView,ForgotPassowrdVerifyOtpView, UserProfileView, LogoutView, ChangePasswordView, UserProfileImageUploadView,UserProfileImageRemoveView,DeactivateAccountView,RefreshTokenView
+from . views import  SignUpView, VerifyEmailView, UserLoginView,ResendOtpView,ResendOtpView,CourseListView,CourseDetailView, CourseVideoView,CreateCheckoutSession, UserEnrolledCourses, WatchCourseView, CheckEnrollmentView, RequestotpView,ForgotPassowrdVerifyOtpView, UserProfileView, LogoutView, ChangePasswordView, UserProfileImageUploadView,UserProfileImageRemoveView,DeactivateAccountView,RefreshTokenView,GoogleSignInView
 from . import views
 
 
 urlpatterns = [ 
+    path('google-signin/',GoogleSignInView.as_view(), name='google-signin'),
     path('signup/', SignUpView.as_view(), name='signup'),
     path('verify-email/', VerifyEmailView.as_view(), name='verify-email'),
     path('resend-otp/', ResendOtpView.as_view(), name='resend-otp'),
@@ -16,12 +17,7 @@ urlpatterns = [
     path('change-password/',ChangePasswordView.as_view(), name='change-password'),
     path('upload-profile-image/', UserProfileImageUploadView.as_view(), name='upload-profile-image'),
     path('remove-profile-image/', UserProfileImageRemoveView.as_view(), name='remove-profile-image'),
-
-    
-
     # path('students-list/',StudentListView.as_view(),name='students-list'),
-    
-
     path('courses/',CourseListView.as_view(), name='course-list'),
     path('course/details/<int:course_id>/', CourseDetailView.as_view(), name='course_detail'),
     path('courses/video/<int:pk>/', CourseVideoView.as_view(), name='course-video'),
@@ -32,7 +28,6 @@ urlpatterns = [
     # path('course/<int:course_id>/reviews/', CourseReviewView.as_view(), name='course-reviews'),
     path('logout/', LogoutView.as_view(), name='auth_logout'),
     path('deactivate-account/',DeactivateAccountView.as_view(), name='user_deactivate'),
-
-     path('webhook/stripe/', views.stripe_webhook, name='stripe-webhook'),
+    path('webhook/stripe/', views.stripe_webhook, name='stripe-webhook'),
 ]
 
