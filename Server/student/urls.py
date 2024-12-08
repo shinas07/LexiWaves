a03c7478e0a051df476148c0cbdfb6e124db8e3d
shinas07 <shinasamanpk@gmail.com>
2024-12-08 13:36:35 +0530

    refactor: update quiz views and add dashboard stats endpoint

--- Server/student/urls.py
+++ Server/student/urls.py
@@ -1,15 +1,15 @@
 from .views import LatestCoursesView
 from django.urls import path
-from .views import QuizView,MarkLessonWatchedView,CompletedLessonsView, CourseEnrolledStudentsView,TutorDetailByCourseView,StudyStreakViewSet, StudyStreakStatus
+from .views import CourseQuizView,MarkLessonWatchedView,CompletedLessonsView, CourseEnrolledStudentsView,TutorDetailByCourseView,StudyStreakViewSet, StudentDashboardStatsView
 
 urlpatterns = [
      path('latest-courses/', LatestCoursesView.as_view(), name='latest-courses'),
      path('mark-lesson-watched/', MarkLessonWatchedView.as_view(), name='mark-lesson-watched'),
-     path('course-quiz/<int:course_id>/', QuizView.as_view(), name='course-quiz'),
-     path('submit-quiz/', QuizView.as_view(), name='submit-quiz'),
+     path('course-quiz/<int:course_id>/', CourseQuizView.as_view(), name='course-quiz'),
+     path('submit-quiz/', CourseQuizView.as_view(), name='submit-quiz'),
      path('completed-lessons/<int:course_id>/', CompletedLessonsView.as_view(), name='completed-lessons'),
      path('students-details/',CourseEnrolledStudentsView.as_view(), name='course-enrolled-student-list'),
      path('tutor-interaction/<int:tutor_id>/', TutorDetailByCourseView.as_view(), name='tutor-detail-by-course'),
      path('study-streak/',StudyStreakViewSet.as_view()),
-     path('check-streak/',StudyStreakStatus.as_view()),
+     path('dashboard-stats/',StudentDashboardStatsView.as_view())
 ]
