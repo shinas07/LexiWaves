046ad8064beb72eebbcbe82f34ed3205e4060286
shinas07 <shinasamanpk@gmail.com>
2024-12-09 21:43:42 +0530

    fix: fixed the require statements to EC Module import error

--- Client/lexiwaves/tailwind.config.js
+++ Client/lexiwaves/tailwind.config.js
@@ -1,7 +1,7 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-const svgToDataUri = require("mini-svg-data-uri");
-const colors = require("tailwindcss/colors");
-const flattenColorPalette = require("tailwindcss/lib/util/flattenColorPalette");
+import defaultTheme from "tailwindcss/defaultTheme";
+import svgToDataUri from "mini-svg-data-uri";
+import colors from "tailwindcss/colors";
+import flattenColorPalette from "tailwindcss/lib/util/flattenColorPalette";
 
 
 /** @type {import('tailwindcss').Config} */
