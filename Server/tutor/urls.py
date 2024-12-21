09ca949b1c1c5b2d92d4efd8b07a925d8a5aadc2
shinas07 <shinasamanpk@gmail.com>
2024-12-21 22:30:32 +0530

    `feat: add new views and update tutor-related API routes for improved functionality

--- Server/tutor/urls.py
+++ Server/tutor/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import  TutorSignUpView, TutorVerifyEmailView, TutorLoginView, TutorDetailsView, CourseCreationViewSet, TutorCreatedCoursesView, EnrolledCoursesView,TutorDashboardView, CompletedLessonsView,CompleteLessonView,QuizCreationView, CourseDetailView, CourseAllDetails, TutorLogoutView, RevenueReportView,TutorProfileView,TutorSlotView,TutorRevenueDetailsView,TutorRevenueView
+from .views import  TutorSignUpView, TutorVerifyEmailView, TutorLoginView, TutorDetailsView, CourseCreationViewSet, TutorCreatedCoursesView, EnrolledCoursesView,TutorDashboardView, CompletedLessonsView,CompleteLessonView,QuizCreationView, CourseDetailView, CourseAllDetails, TutorLogoutView, RevenueReportView,TutorProfileView,TutorSlotView,TutorRevenueDetailsView,TutorDashboardDetailsView,TutorResendOtpView,TutorApprovalStatusView
 
 urlpatterns = [
     path('signup/', TutorSignUpView.as_view(), name='tutor_signup'),
     path('verify_email/', TutorVerifyEmailView.as_view(), name='verify_email'),
+    path('resend-otp/', TutorResendOtpView.as_view(), name='resend-otp'),
     path('login/', TutorLoginView.as_view(), name='tutor-login'),
     path('details/',TutorDetailsView.as_view(), name='tutor-details'),
+    path('approval-status/',TutorApprovalStatusView.as_view(), name='approval-status'),
     path('dashboard/',TutorDashboardView.as_view(), name='tutor-dashboard'),
     path('profile/', TutorProfileView.as_view(), name='tutor-profile'),
     path('courses-create/', CourseCreationViewSet.as_view({'post': 'create'}), name='create-course'),
@@ -18,7 +20,7 @@ urlpatterns = [
     path('course/details/<int:pk>/', CourseDetailView.as_view(), name='course-detail'), 
     path('qiuz-course/details/<int:pk>/',CourseAllDetails.as_view(), name='all-course-details'),
     path('revenue-report/', RevenueReportView.as_view(), name='revenue_report'),
-    path('revenue/', TutorRevenueView.as_view(), name='tutor-revenue'),
+    path('dashboard-details/', TutorDashboardDetailsView.as_view(), name='tutor-revenue'),
     path('slot/update/',TutorSlotView.as_view(),name='tutor-slot-update'),
     path('revenue-details/',TutorRevenueDetailsView.as_view()),
     path('logout/',TutorLogoutView.as_view(), name='tutor-logout'),
