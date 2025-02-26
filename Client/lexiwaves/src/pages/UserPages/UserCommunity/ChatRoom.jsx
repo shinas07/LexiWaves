0729bd4853e034b99aa76dd5b097084f4a8a7409
shinas07 <shinasamanpk@gmail.com>
2025-02-26 21:03:42 +0530

    refactor: Update WebSocket connection logic and cleanup console logs in ChatRoom.

--- Client/lexiwaves/src/pages/UserPages/UserCommunity/ChatRoom.jsx
+++ Client/lexiwaves/src/pages/UserPages/UserCommunity/ChatRoom.jsx
@@ -52,17 +52,17 @@ const ChatRoom = () => {
             return;
         }
         const token = localStorage.getItem('accessToken')
+        socket.current = new WebSocket(`ws://127.0.0.1:8000/ws/chat/${language}/?token=${token}`);
 
+
+         // socket.current = new WebSocket(`wss://api.lexiwaves.online/ws/chat/${language}/?token=${token}`);
+         
         // const wsUrl = process.env.NODE_ENV === 'production' 
         // ? `wss://api.lexiwaves.online/ws/chat/${language}/?token=${token}`
         // : `ws://127.0.0.1:8000/ws/chat/${language}/?token=${token}`;
 
-        // socket.current = new WebSocket(`ws://127.0.0.1:8000/ws/chat/${language}/?token=${token}`); for locall connection
-        socket.current = new WebSocket(`wss://api.lexiwaves.online/ws/chat/${language}/?token=${token}`);
-
         socket.current.onopen = () => {
             setIsConnected(true);
-            console.log('WebSocket Connected');
         };
 
         socket.current.onmessage = (event) => {
@@ -88,7 +88,6 @@ const ChatRoom = () => {
 
         socket.current.onclose = () => {
             setIsConnected(false);
-            console.log('WebSocket Disconnected');
         };
 
         return () => {
