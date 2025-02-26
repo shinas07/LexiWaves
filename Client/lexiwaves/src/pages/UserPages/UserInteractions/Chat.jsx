9907c3055c9d0be6f03d26bb506a3fd92edc64d5
shinas07 <shinasamanpk@gmail.com>
2025-02-26 21:16:28 +0530

    refactor: add authentication token to websocket connection

--- Client/lexiwaves/src/pages/UserPages/UserInteractions/Chat.jsx
+++ Client/lexiwaves/src/pages/UserPages/UserInteractions/Chat.jsx
@@ -90,8 +90,8 @@ const ChatSection = ({ roomId }) => {
       const token = localStorage.getItem('accessToken');
       if (!token || !roomId) return;
 
-      // websocket.current = new WebSocket(`ws://127.0.0.1:8000/ws/classchat/${roomId}/`);
-      websocket.current = new WebSocket(`wss://api.lexiwaves.online/ws/classchat/${roomId}/`);
+      // websocket.current = new WebSocket(`ws://127.0.0.1:8000/ws/classchat/${roomId}/?token=${token}`);
+      websocket.current = new WebSocket(`wss://api.lexiwaves.online/ws/classchat/${roomId}/?token=${token}`);
       
       websocket.current.onopen = () => {
         setIsConnected(true);
